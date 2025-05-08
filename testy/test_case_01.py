e8d19405b8c9d95ed53e316ff285ece7746eb412
DEV-COM <kwp@tlen.pl>
2025-05-08 16:35:37 +0200

    Dodano testy jednostkowe dla funkcji dodawania oraz zaimplementowano funkcję dodawania w nowym pliku calculator.py

--- testy/test_case_01.py
+++ testy/test_case_01.py
@@ -1,1 +1,18 @@
-print("witaj z github actions!")
+import unittest
+from calculator import add
+
+class TestCalculator(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        result = add(2, 3)
+        self.assertEqual(result, 5)
+        
+    def test_add_negative_numbers(self):
+        result = add(-2, -3)
+        self.assertEqual(result, -5)
+        
+    def test_add_mixed_numbers(self):
+        result = add(-2, 3)
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
